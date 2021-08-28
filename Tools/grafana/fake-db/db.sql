3820f387d4cb7e368e50bbbba46c2260c3085535
Wenwei Tao <wenweitao@wenweis-mbp.lan>
2021-08-28 15:09:46 -0700

    Added GitHub mock

---
+++ Tools/grafana/fake-db/db.sql
@@ -0,0 +1,52 @@
+/* team name and id table */
+CREATE TABLE `scrum_teams` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `team_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `team_name` (`team_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+
+/* team member name and id table */
+CREATE TABLE `scrum_members` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `start_time` datetime NOT NULL COMMENT 'start time',
+  `end_time` datetime DEFAULT NULL COMMENT 'end time',
+  `member_name` varchar(255) NOT NULL COMMENT 'name',
+  `team` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `member_name` (`member_name`),
+  KEY `team` (`team`),
+  CONSTRAINT `scrum_members_ibfk_1` FOREIGN KEY (`team`) REFERENCES `scrum_teams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
+
+
+/* git daily code change table */
+CREATE TABLE `git_daily_status` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `the_date` datetime NOT NULL,
+  `member_name` int NOT NULL,
+  `team` int NOT NULL,
+  `lines_added` int DEFAULT 0,
+  `lines_removed` int DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `member_name` (`member_name`),
+  KEY `team` (`team`),
+  CONSTRAINT `git_daily_status_ibfk_1` FOREIGN KEY (`member_name`) REFERENCES `scrum_members` (`id`),
+  CONSTRAINT `git_daily_status_ibfk_2` FOREIGN KEY (`team`) REFERENCES `scrum_teams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=505 DEFAULT CHARSET=utf8
+
+/* github pull request table */
+CREATE TABLE `github_pull_requests` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `created_at` datetime NOT NULL COMMENT 'create time',
+  `closed_at` datetime DEFAULT NULL COMMENT 'close time',
+  `pr_num` int NOT NULL,
+  `member_name` int NOT NULL,
+  `team` int NOT NULL,
+  `state` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `member_name` (`member_name`),
+  KEY `team` (`team`),
+  CONSTRAINT `github_pull_requests_ibfk_1` FOREIGN KEY (`member_name`) REFERENCES `scrum_members` (`id`),
+  CONSTRAINT `github_pull_requests_ibfk_2` FOREIGN KEY (`team`) REFERENCES `scrum_teams` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
--- Tools/grafana/fake-db/dummyWriter.py
+++ Tools/grafana/fake-db/dummyWriter.py
@@ -19,6 +19,21 @@ teamMemberNames = [
         'Dylan'
     ]
 ]
+teamMemberPRs = [
+    [
+        4,
+        1,
+        3,
+        3,
+        6
+    ],
+    [
+        2,
+        2,
+        4,
+        1
+    ]
+]
 numOfDays = 56
 startDate = datetime.datetime(2020, 12, 12) 
 
@@ -48,19 +63,19 @@ class RecordSourceCode(RecordBase):
         memberId = self.getScrumMemberId(member)
         teamId = self.getScrumTeamId(team)
 
-        sql = f'SELECT * FROM git_daily_status WHERE the_date = "{date}" AND member_name = "{memberId}" AND team = "{teamId}"'
+        sql = f'SELECT * FROM git_daily_status WHERE the_date = "{date}" AND member_name = {memberId} AND team = {teamId}'
         self.cursor.execute(sql)
         self.cursor.fetchone()
         if self.cursor.rowcount <= 0:
             sql = 'INSERT INTO git_daily_status ' +\
                 '(the_date, member_name, team, lines_added, lines_removed) ' +\
-                f'VALUES ("{date}", "{memberId}", "{teamId}", {linesAdded}, {linesRemoved})'
+                f'VALUES ("{date}", {memberId}, {teamId}, {linesAdded}, {linesRemoved})'
         else:
             sql = 'UPDATE git_daily_status ' +\
                 f'SET ' +\
                 f'  lines_added = {linesAdded}, ' +\
                 f'  lines_removed = {linesRemoved} ' +\
-                f'WHERE the_date = "{date}" AND member_name = "{memberId}"'
+                f'WHERE the_date = "{date}" AND member_name = {memberId}'
         self.cursor.execute(sql)
         self.cnx.commit()
 
@@ -73,6 +88,48 @@ class RecordSourceCode(RecordBase):
                     linesRemoved = int(random.random() * 200)
                     self.writeSourceCodeRecord(date, teamMemberNames[team][member], teamNames[team], linesAdded, linesRemoved)
             date += datetime.timedelta(days = 1)
+
+class RecordGithub(RecordBase):
+    def writePullRequestRecord(self, created_at, closed_at, pr_num, member, team, state):
+        memberId = self.getScrumMemberId(member)
+        teamId = self.getScrumTeamId(team)
+
+        sql = f'SELECT * FROM github_pull_requests WHERE pr_num = "{pr_num}"'
+        self.cursor.execute(sql)
+        self.cursor.fetchone()
+        if self.cursor.rowcount <= 0:
+            if state == 'open':
+                sql = 'INSERT INTO github_pull_requests ' +\
+                    '(created_at, closed_at, pr_num, member_name, team, state) ' +\
+                    f'VALUES ("{created_at}", NULL, {pr_num}, {memberId}, {teamId}, "{state}")'
+            else:
+                sql = 'INSERT INTO github_pull_requests ' +\
+                    '(created_at, closed_at, pr_num, member_name, team, state) ' +\
+                    f'VALUES ("{created_at}", "{closed_at}", {pr_num}, {memberId}, {teamId}, "{state}")'
+        else:
+            if state == 'open':
+                sql = 'UPDATE github_pull_requests ' +\
+                    f'SET created_at = "{created_at}", closed_at = NULL, state = "{state}" WHERE pr_num = {pr_num}'
+            else:
+                sql = 'UPDATE github_pull_requests ' +\
+                    f'SET created_at = "{created_at}", closed_at = "{closed_at}", state = "{state}" WHERE pr_num = {pr_num}'
+        self.cursor.execute(sql)
+        self.cnx.commit()
+
+    def createRecords(self):
+        pr_num = 0
+        for team in range(len(teamNames)):
+            for member in range(len(teamMemberNames[team])):
+                for _ in range(teamMemberPRs[team][member]):
+                    created_at = startDate + datetime.timedelta(days = int(random.random() * 4))
+                    flip = random.randint(0, 1)
+                    state = 'open'
+                    closed_at = None
+                    if flip == 0:
+                        state = 'closed'
+                        closed_at = created_at + datetime.timedelta(hours = int(random.random() * 48) + 1)
+                    self.writePullRequestRecord(created_at, closed_at, pr_num, teamMemberNames[team][member], teamNames[team], state)
+                    pr_num += 1
 
 class RecordSprint(RecordBase):
     def writeSprintRecord(self, date, name, team, totalTickets, totalStoryPotins, completedTickets, completedStoryPoints):
@@ -117,7 +174,10 @@ try:
     cursor = cnx.cursor()
 
     # write source code activities per team member
-    rec = RecordSourceCode(cnx, cursor)
+    #rec = RecordSourceCode(cnx, cursor)
+    #rec.createRecords()
+
+    rec = RecordGithub(cnx, cursor)
     rec.createRecords()
 
     # write sprint ticket and story activities per team
